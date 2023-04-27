b850ea69c73d742146da7c7f2abd88d51136645d
Fabricio <fabriciocolombo0096@gmail.com>
2023-04-27 18:38:00 -0300

    django2

--- cad_usuarios/app_Cadastro_Usuarios/views.py
+++ cad_usuarios/app_Cadastro_Usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Usuario
 
 # Create your views here.
 
@@ -6,4 +7,14 @@ def home(request):
     return render(request,'usuarios/home.html')
 
 def usuarios(request):
-    pass
+    #salvar os dados da tela para o banco de dados
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    # exibir todos os usuarios ja cadastrados em uma nova pagina
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+    #retornar os dados para a pagina de listagem
+    return render(request, 'usuarios/usuarios.html', usuarios)
